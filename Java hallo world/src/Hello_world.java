aa28b4e8ee4a620eb98352d152594264f4cdbe98
Huhich <124464928+huh150@users.noreply.github.com>
2024-09-09 10:38:42 +0700

    Add files via upload

---
+++ Java hallo world/src/Hello_world.java
@@ -0,0 +1,28 @@
+public class Hello_world
+{
+    public static void main(String[] args)
+    {
+        // переменные целочисленные //
+        byte A1 = 10;
+        short A2 = 10000;
+        int A3 = 1000;
+        long A4 = 10000;
+
+        // переменные символьный //
+        char B1 = 'd';
+
+        // переменные с плавающей точкой //
+        float C1 = 1.4F;
+        double C2 = 2.432;
+
+        // переменные логический тип //
+        boolean D1 = true;
+
+        // массив //
+        int[] G1 = {1,3,44,2};
+
+
+        String F = "Hello world";
+        System.out.println(F);
+    }
+}
